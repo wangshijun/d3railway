3ab4ae4f525ee41465d25aff8f3215980f7fa62e
wangshijun <wangshijun2010@gmail.com>
2015-06-22 16:23:20 +0800

    add download api

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var sys = require('sys'),
+    tmp = require('tmp'),
+    fs = require('fs'),
+    exec = require('child_process').exec;
+
 /**
  * Module dependencies.
  */
@@ -7,5 +12,42 @@ exports.index = function(req, res) {
     res.render('index', {
         user: req.user || null,
         request: req
+    });
+};
+
+exports.download = function (req, res) {
+    // output format (pdf or png )
+
+    tmp.file({postfix: '.svg'}, function _tempFileCreated(err, inputFilePath, fd) {
+
+        if (err) {
+            res.json(500, err);
+        } else {
+            fs.writeFile(inputFilePath, req.body.data, function(err) {
+                if (err) {
+                    res.json(500, err);
+                } else {
+                    tmp.file({postfix: '.' + req.body.format}, function _tempFileCreated(err, outputFilePath, fd) {
+                        if (err) {
+                            res.json(500, err);
+                        } else {
+                            var cmd = "rsvg-convert -z 5 --background-color white -a";
+                            cmd += " -f " + req.body.format;
+                            cmd += " -o " + outputFilePath;
+                            cmd += " " + inputFilePath;
+
+                            exec(cmd, function (error, stdout, stderr) {
+                                if (error !== null) {
+                                    res.json(500, error);
+                                } else {
+                                    res.attachment(outputFilePath);
+                                    res.sendfile(outputFilePath);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
     });
 };
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
     // Root routing
     var core = require('../../app/controllers/core.server.controller');
     app.route('/').get(core.index);
+    app.route('/api/download').post(core.download);
 };
