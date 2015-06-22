d2fbbeec2962ab4b566c1d407877b7e2d75aded2
wangshijun <wangshijun2010@gmail.com>
2015-06-22 18:21:14 +0800

    update api

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -18,29 +18,31 @@ exports.index = function(req, res) {
 };
 
 exports.download = function (req, res) {
+    console.log(req.body);
+
     // output format (svg or png )
     tmp.file({postfix: '.svg'}, function _tempFileCreated(err, inputFilePath, fd) {
-        if (err) { return res.json(500, err); }
+        if (err) { return res.status(500).jsonp({ error: err, stage: 'tmp' }); }
 
         fs.writeFile(inputFilePath, req.body.data, function (err) {
-            if (err) { return res.json(500, err); }
+            if (err) { return res.status(500).jsonp({ error: err, stage: 'writetmp' }); }
 
             Svg.fromSvgDocument(inputFilePath, function (err, svg) {
-                if (err) { return res.json(500, err); }
+                if (err) { return res.status(500).jsonp({ error: err, stage: 'fromsvg' }); }
 
                 var outputFilePath = './' + (new Date).getTime() + '.' + req.body.format;
 
                 if (req.body.format === 'svg') {
                     svg.save({ output : outputFilePath }, function (err, filename) {
-                        if (err) { return res.json(500, err); }
+                        if (err) { return res.status(500).jsonp({ error: err, stage: 'writesvg' }); }
                         res.attachment(outputFilePath);
                         res.sendfile(outputFilePath);
                     });
                 } else {
                     svg.savePng({ output : outputFilePath }, function (err, filename) {
-                        if (err) { return res.json(500, err); }
+                        if (err) { return res.status(500).jsonp({ error: err, stage: 'writepng' }); }
                         res.attachment(outputFilePath);
-                        res.sendfile(outputFilePath);
+                        res.sendFile(outputFilePath);
                     });
                 }
             });
